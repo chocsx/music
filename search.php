b68f7ebad557f8acfeb280f806632f5d9154fed0
Alashov Berkeli <yunus.alashow@gmail.com>
2016-07-05 05:59:18 +0500

    search.php bug fix and refactor

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php 
 /* ========================================================================
- * Music v1.3.7
+ * Music v1.4.0
  * https://github.com/alashow/music
  * ======================================================================== */
 
@@ -21,10 +21,16 @@ if (isset($params["callback"])) {
 
 $fullUrl = $apiUrl . http_build_query($params);
 
+if ($config['isNoCache']) {
+    removeCacheForUrl($fullUrl);
+}
+
 $result = file_get_contents_with_cache($fullUrl, true);
 
-if (!empty($result["error"])) {
-  removeCacheForUrl($fullUrl);
+$resultJson = json_decode($result);
+
+if (isset($resultJson->error) || ! empty($resultJson->response)) {
+  	removeCacheForUrl($fullUrl);
 }
 
 if (isset($originalJsonCallback)) {
